9fa474e243df4a08ca5442bf0cb2c2680a0f1860
kadirbelen <61799635+kadirbelen@users.noreply.github.com>
2021-05-12 01:10:02 +0300

    Hafta_0_3

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
@@ -0,0 +1,39 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name="job_titles")
+public class JobTitle {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="job_title")
+	private String jobTitle;
+	
+	public JobTitle() {
+		
+	}
+
+	public JobTitle(int id, String jobTitle) {
+		super();
+		this.id = id;
+		this.jobTitle = jobTitle;
+	}
+	
+	
+	
+
+}
